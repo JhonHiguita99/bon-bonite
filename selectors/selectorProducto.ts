603d4c149f0d634e9b4e779976b5544483ab277f
JhonHiguita99 <jhon.higuita.1999@hotmail.com>
2026-03-16 11:21:05 -0500

    cambio selectores

--- selectors/selectorProducto.ts
+++ selectors/selectorProducto.ts
@@ -2,9 +2,9 @@ import { test, Page, Browser, expect, Locator } from '@playwright/test';
 const selectorsProductos = {
 
     locatorFiltro: `//span[contains(text(), 'Filtros')]`,
-    locatorImg: `//img[@id="image-1017432"]`,
+    locatorImg: `//img[@id="image-1019284"]`,
     locatorTalla: `//button[contains(text(), '35')]`,
-    locatorH1NombreProductos: `//h1[contains(text(), 'Baleta en malla con piedras de fantasía blanco puro')]`,
+    locatorH1NombreProductos: `//h1[contains(text(), 'Tacón en cuero charolado beige')]`,
     locatorAceptar:`//div[@id='cookiescript_accept']`,
     locatorCarrito: `//button[@class='uppercase bg-black border-2 border-black w-full py-2 block font-semibold text-sm text-white text-center transition-all hover:bg-white hover:text-black single_add_to_cart_button button alt']`
 
