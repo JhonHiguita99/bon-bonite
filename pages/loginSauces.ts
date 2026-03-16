32e05d68c1966028c4ce4236ee9392ced0584ef7
JhonHiguita99 <jhon.higuita.1999@hotmail.com>
2026-03-16 11:33:10 -0500

    cambio

--- pages/loginSauces.ts
+++ pages/loginSauces.ts
@@ -11,12 +11,18 @@ export class PageLoginSauces{
     }
 
     async hacerLogin(testInfo: { attach: Function}){
-        await this.page.waitForLoadState('networkidle');
-        await expect(this.page.locator(selectorsLoginSauces.locatorUser)).toBeEditable({timeout: 15000});
-        await this.page.locator(selectorsLoginSauces.locatorUser).fill('standard_user');
-        await this.page.locator(selectorsLoginSauces.locatorPaaword).fill('secret_sauce');
-        await expect(this.page.locator(selectorsLoginSauces.locatorBotonLogin)).toBeVisible({timeout: 15000});
-        await this.page.locator(selectorsLoginSauces.locatorBotonLogin).click({force:true});
+        const username = this.page.locator(selectorsLoginSauces.locatorUser);
+        const password = this.page.locator(selectorsLoginSauces.locatorPaaword);
+        const loginBtn = this.page.locator(selectorsLoginSauces.locatorBotonLogin);
+
+        await expect(username).toBeVisible({ timeout: 15000 });
+        await username.fill('standard_user');
+
+        await expect(password).toBeVisible();
+        await password.fill('secret_sauce');
+
+        await expect(loginBtn).toBeEnabled();
+        await loginBtn.click();
         
     }
 }
